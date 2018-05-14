5517a8d2d7481665516c145564e730023c125c06
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-05-14 13:51:41 +0200

    Cosmetics

--- onegov/gazette/views/notices.py
+++ onegov/gazette/views/notices.py
@@ -1,21 +1,15 @@
-from datetime import datetime
-from io import BytesIO
 from morepath import redirect
-from morepath.request import Response
 from onegov.core.security import Personal
 from onegov.core.security import Private
-from onegov.core.utils import normalize_for_url
 from onegov.gazette import _
 from onegov.gazette import GazetteApp
 from onegov.gazette.collections import GazetteNoticeCollection
-from onegov.gazette.collections.notices import TRANSLATIONS
 from onegov.gazette.forms import EmptyForm
 from onegov.gazette.forms import NoticeForm
 from onegov.gazette.layout import Layout
 from onegov.gazette.models import GazetteNotice
 from onegov.gazette.views import get_user
 from onegov.gazette.views import get_user_and_group
-from xlsxwriter import Workbook
 
 
 @GazetteApp.form(
@@ -151,86 +145,6 @@ def view_notices(self, request):
         'clear': request.link(self.for_dates(None, None).for_term(None)),
         'new_notice': request.link(self, name='new-notice')
     }
-
-
-@GazetteApp.html(
-    model=GazetteNoticeCollection,
-    template='statistics.pt',
-    name='statistics',
-    permission=Private
-)
-def view_notices_statistics(self, request):
-    """ View the list of notices.
-
-    This view is only visible by a publisher. This (in the state 'accepted')
-    is the view used by the publisher.
-
-    """
-
-    layout = Layout(self, request)
-    filters = (
-        {
-            'title': _(state),
-            'link': request.link(self.for_state(state), name='statistics'),
-            'class': 'active' if state == self.state else ''
-        }
-        for state in (
-            'drafted', 'submitted', 'accepted', 'rejected', 'published'
-        )
-    )
-
-    return {
-        'layout': layout,
-        'filters': filters,
-        'collection': self,
-        'title': _("Statistics"),
-        'from_date': self.from_date,
-        'to_date': self.to_date,
-        'by_organizations': self.count_by_organization(),
-        'by_category': self.count_by_category(),
-        'by_groups': self.count_by_group(),
-        'clear': request.link(self.for_dates(None, None), name='statistics')
-    }
-
-
-@GazetteApp.view(
-    model=GazetteNoticeCollection,
-    name='statistics-xlsx',
-    permission=Private
-)
-def view_notices_statistics_xlsx(self, request):
-    """ View the statistics as XLSX. """
-
-    output = BytesIO()
-    workbook = Workbook(output)
-    for title, row, content in (
-        (_("Organizations"), _("Organization"), self.count_by_organization),
-        (_("Categories"), _("Category"), self.count_by_category),
-        (_("Groups"), _("Group"), self.count_by_group),
-    ):
-        worksheet = workbook.add_worksheet()
-        worksheet.name = request.translate(title)
-        worksheet.write_row(0, 0, (
-            request.translate(row),
-            request.translate(_("Number of Notices"))
-        ))
-        for index, row in enumerate(content()):
-            worksheet.write_row(index + 1, 0, row)
-    workbook.close()
-    output.seek(0)
-
-    response = Response()
-    response.content_type = (
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    )
-    response.content_disposition = 'inline; filename={}-{}-{}.xlsx'.format(
-        request.translate(_("Statistics")).lower(),
-        normalize_for_url(request.translate(TRANSLATIONS.get(self.state, ''))),
-        datetime.utcnow().strftime('%Y%m%d%H%M')
-    )
-    response.body = output.read()
-
-    return response
 
 
 @GazetteApp.form(
