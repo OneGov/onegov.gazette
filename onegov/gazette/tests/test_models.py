d79aa6889bb266eb8df8225a5acc80c254451245
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2019-01-31 08:50:33 +0100

    Fixes pep8 errors

--- onegov/gazette/tests/test_models.py
+++ onegov/gazette/tests/test_models.py
@@ -488,7 +488,7 @@ def test_notice_organization(session):
     session.flush()
     assert notice.organization_id == 'xxx'
     assert notice.organization_object == organization
-    assert notice.organization is 'Organization'  # through title observer
+    assert notice.organization == 'Organization'  # through title observer
 
 
 def test_notice_category(session):
@@ -505,7 +505,7 @@ def test_notice_category(session):
     session.flush()
     assert notice.category_id == 'xxx'
     assert notice.category_object == category
-    assert notice.category is 'Category'  # through title observer
+    assert notice.category == 'Category'  # through title observer
 
 
 def test_notice_issues(session):
